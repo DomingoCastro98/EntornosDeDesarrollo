22c915de91aab954a3d6a6e85a2c0b2b43ec5ce8
Ismael Aguilar <iaguarj0808@iesmarquesdecomares.org>
2024-10-22 12:49:58 +0200

    Primera subida Ismael

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -4,6 +4,7 @@ public class HolaProfe {
 
 	public static void main(String[] args) {
 		System.out.println("Hola, mi nombre es Domingo Castro");
+		System.out.println("Hola mi nombre es Ismael Aguilar");
 
 	}
 
