a3beeb3ea02617fda779e3c32e35a4c2f294fd21
matilde <mjimbed098@iesmarquesdecomares>
2024-10-22 13:00:59 +0200

    Agregado nombre Matilde

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -5,6 +5,7 @@ public class HolaProfe {
 	public static void main(String[] args) {
 		System.out.println("Hola, mi nombre es Domingo Castro");
 		System.out.println("Hola mi nombre es Ismael Aguilar");
+		System.out.println("Hola mi nombre es Matilde Jimenez");
 
 	}
 
