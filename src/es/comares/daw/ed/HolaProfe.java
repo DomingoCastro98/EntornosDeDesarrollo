66909690d00e4a3292d630b51cd3cb34ec101027
matilde <matildejimenezbedmar@gmail.com>
2024-10-25 08:56:48 +0200

    FelizNavidad

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -8,6 +8,8 @@ public class HolaProfe {
 		System.out.println("Hola mi nombre es Matilde Jimenez");
 		System.out.println("Buenas, soy Manolo. Ahora debéis obtener los cambios desde Eclipse. No olividéis documentarlo con capturas de pantalla ");
 		System.out.println("Hoy voy a suspender programación, ayudame por favor");
+		System.out.println("Ahora hacemos un commir by Matilde");
+		
 	}
 
 }
