e5a7f2a07fe67750b496679021462104b33ce97a
Ismael Aguilar <iaguarj0808@iesmarquesdecomares.org>
2024-10-25 08:46:19 +0200

    Nueva Subida

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -7,7 +7,7 @@ public class HolaProfe {
 		System.out.println("Hola mi nombre es Ismael Aguilar");
 		System.out.println("Hola mi nombre es Matilde Jimenez");
 		System.out.println("Buenas, soy Manolo. Ahora debéis obtener los cambios desde Eclipse. No olividéis documentarlo con capturas de pantalla ");
-
+		System.out.println("Hoy voy a suspender programación, ayudame por favor");
 	}
 
 }
